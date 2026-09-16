66076308c1bf1daf1d4a57a8bb776e3e2f1a06bf
harshk200610-cloud <harshk.200610@gmail.com>
2026-09-16 21:31:41 +0530

    developed basic of the home page

---
+++ src/components/home/HomeHeroText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeHeroText = () => {
+  return (
+      <div className='font-[font1] pt-5 text-center text-white'>
+        <div className='text-[9.5vw] uppercase leading-[8vw]'>The spark for</div>
+      <div className='text-[9.5vw] uppercase leading-[8vw]'>allthings</div>
+      <div className='text-[9.5vw] uppercase leading-[8vw]'>creative</div>
+    </div>
+  )
+}
+
+export default HomeHeroText
